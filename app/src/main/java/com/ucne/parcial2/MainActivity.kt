d73836232a5b30606a8dc9ee123992d947401366
Harlit <harlit.estevez@gmail.com>
2023-03-20 19:18:29 -0400

    Todo Listo y solucinado

--- app/src/main/java/com/ucne/parcial2/MainActivity.kt
+++ app/src/main/java/com/ucne/parcial2/MainActivity.kt
@@ -6,23 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.ucne.parcial2.ui.theme.Parcial2Theme
 import android.os.Build
-import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.material.*
 import androidx.compose.material3.*
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.ucne.parcial2.ui.theme.Ticket.TicktetListScreen
-import com.ucne.parcial2.ui.theme.Tickets.TicketsScreen
-import com.ucne.parcial2.ui.theme.navegacion.DrawerMenu
-import com.ucne.parcial2.ui.theme.navegacion.ScreenModule
+import com.ucne.parcial2.ui.theme.navegacion.HostList
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -32,42 +22,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Parcial2Theme() {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val navController = rememberNavController()
-                    NavHost(
-                        navController = navController,
-                        startDestination = ScreenModule.Start.route
-                    ) {
-                        composable(ScreenModule.Start.route) {
-                            DrawerMenu(navController = navController)
-                        }
-                        composable(ScreenModule.TicketsList.route) {
-                            TicktetListScreen(onNewTicket = {}, navController = navController)
-                        }
-                        composable(ScreenModule.Tickets.route) {
-                            TicketsScreen(navController = navController)
-                        }
-                    }
+                    HostList()
                 }
             }
         }
-    }
-}
-
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Parcial2Theme {
-        Greeting("Android")
     }
 }
